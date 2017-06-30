562b6b11a19e7399a7d83ba83436a70be5001246
Plinio Almeida <plinio@eptus.com.br>
2017-06-30 10:43:24 -0400

    Realizando primeiro login

--- resources/assets/js/controllers/login.js
+++ resources/assets/js/controllers/login.js
@@ -2,14 +2,18 @@
  * Created by Administrador on 30/06/17.
  */
 angular.module('app.controllers')
-    .controller('LoginController',['$scope',function($scope){
+    .controller('LoginController',['$scope','$location','OAuth',function($scope,$location,OAuth){
     $scope.user = {
         username: '',
         password: ''
     };
 
     $scope.login = function() {
-
+        OAuth.getAccessToken($scope.user).then(function(){
+            $location.path('home');
+        },function(){
+            alert('Login Inválido');
+        });
     };
 
 }]);
